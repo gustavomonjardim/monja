6bbc50ca743612411e7f2149c4be44a41f5b0949
Gustavo Monjardim <gustavo.monjardim@liftbank.com.br>
2020-03-28 19:19:33 -0300

    feat: adiciona prop style no renderer

--- src/monja/renderer.js
+++ src/monja/renderer.js
@@ -1,41 +1,60 @@
 function updateDomProperties(dom, prevProps, nextProps) {
   const isEvent = key => key.startsWith("on");
-  const isProperty = key => key !== "children" && !isEvent(key);
+  const isStyle = key => key === "style";
+  const isProperty = key =>
+    key !== "children" && !isEvent(key) && !isStyle(key);
   const isNew = (prev, next) => key => prev[key] !== next[key];
   const isGone = next => key => !(key in next);
 
   // Remove old or changed event listeners
   Object.keys(prevProps)
     .filter(isEvent)
-    .filter(key => !(key in nextProps) || isNew(prevProps, nextProps)(key))
-    .forEach(name => {
-      const eventType = name.toLowerCase().substring(2);
-      dom.removeEventListener(eventType, prevProps[name]);
+    .filter(key => isGone(nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach(eventName => {
+      const eventType = eventName.toLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[eventName]);
     });
 
   // Add event listeners
   Object.keys(nextProps)
     .filter(isEvent)
     .filter(isNew(prevProps, nextProps))
-    .forEach(name => {
-      const eventType = name.toLowerCase().substring(2);
-      dom.addEventListener(eventType, nextProps[name]);
+    .forEach(eventName => {
+      const eventType = eventName.toLowerCase().substring(2);
+      dom.addEventListener(eventType, nextProps[eventName]);
+    });
+
+  const prevStyle = prevProps.style ?? {};
+  const nextStyle = nextProps.style ?? {};
+
+  // Remove old styles
+  Object.keys(prevStyle)
+    .filter(isGone(prevStyle, nextStyle))
+    .forEach(styleName => {
+      dom.style[styleName] = "";
+    });
+
+  // Add new styles
+  Object.keys(nextStyle)
+    .filter(isNew(prevStyle, nextStyle))
+    .forEach(styleName => {
+      dom.style[styleName] = nextStyle[styleName];
     });
 
   // Remove old properties
   Object.keys(prevProps)
     .filter(isProperty)
     .filter(isGone(nextProps))
-    .forEach(name => {
-      dom[name] = "";
+    .forEach(propName => {
+      dom[propName] = "";
     });
 
   // Set new or changed properties
   Object.keys(nextProps)
     .filter(isProperty)
     .filter(isNew(prevProps, nextProps))
-    .forEach(name => {
-      dom[name] = nextProps[name];
+    .forEach(propName => {
+      dom[propName] = nextProps[propName];
     });
 }
 
